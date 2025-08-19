6bfe89d13c0acc7a610819bd0b8e1d001f6ae3d2
ChivuAndrei2003 <chivuandrei2003@gmail.com>
2025-08-19 17:03:48 +0300

    update

---
+++ src/index.js
@@ -0,0 +1,23 @@
+class linkedList {
+  constructor(head, size) {
+    this.head = null;
+    this.size = 0;
+  }
+  append(value) {}
+  prepend(value) {}
+  size() {}
+  head() {}
+  tail() {}
+  at(index) {}
+  pop() {}
+  contains(value) {}
+  find(value) {}
+  toString() {}
+}
+
+class Node {
+  constructor(value, nextNode) {
+    this.value = value;
+    this.nextNode = null;
+  }
+}
