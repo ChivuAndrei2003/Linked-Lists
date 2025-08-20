965fbc2fd61c327dbdd66b7b09a0dd7a5fd55ddc
Chivu Andrei <chivuandrei2003@gmail.com>
2025-08-20 18:31:39 +0300

    made some changes to index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,73 @@
+class Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 class linkedList {
   constructor(head, size) {
     this.head = null;
     this.size = 0;
   }
-  append(value) {}
-  prepend(value) {}
-  size() {}
-  head() {}
-  tail() {}
-  at(index) {}
-  pop() {}
+
+  append(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+    this.size++;
+  }
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.size++;
+  }
+  size() {
+    console.log(` The list has ${this.size} nodes`);
+  }
+  head() {
+    console.log(`${this.head}`);
+  }
+  tail() {
+    let cur = this.head;
+  }
+  at(value) {
+
+  }
+  pop() {
+
+  }
   contains(value) {}
-  find(value) {}
-  toString() {}
+  find(value) {
+    let current = this.head;
+    let index = 0;
+
+    while (current) {
+      if (current.value === value) {
+        return index;
+      }
+      current = current.next;
+      index++;
+    }
+    return null;
+  }
+
+  toString() {
+  
+}
 }
 
-class Node {
-  constructor(value, nextNode) {
-    this.value = value;
-    this.nextNode = null;
-  }
-}
+const list = new linkedList();
+list.append("caine");
+list.append("pisica");
+console.log(list);
