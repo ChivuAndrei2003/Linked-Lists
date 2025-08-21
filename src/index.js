3bad6fedd189db3407f84c37164931f785f86a68
ChivuAndrei2003 <chivuandrei2003@gmail.com>
2025-08-21 15:43:41 +0300

    small update for index.js

--- src/index.js
+++ src/index.js
@@ -26,28 +26,51 @@ class linkedList {
     }
     this.size++;
   }
+
   prepend(value) {
     const newNode = new Node(value);
     newNode.next = this.head;
     this.head = newNode;
     this.size++;
   }
+
   size() {
     console.log(` The list has ${this.size} nodes`);
   }
+
   head() {
     console.log(`${this.head}`);
   }
+
   tail() {
-    let cur = this.head;
-  }
-  at(value) {
+    if (!this.head) return null;
+    let current = this.head;
 
+    while (current.next) {
+      current = current.next;
+    }
+    console.log(current.value);
   }
+
+  at(value) {}
+
   pop() {
-
+    let current = this.head;
+    let previous;
+   
+    if (this.size === 0) return;
+    else {
+      while (current.next != null) {
+        previous = current;
+        current = current.next;
+      }
+      previous.next = null;
+    }
+    this.size--; 
   }
+
   contains(value) {}
+
   find(value) {
     let current = this.head;
     let index = 0;
@@ -62,12 +85,12 @@ class linkedList {
     return null;
   }
 
-  toString() {
-  
-}
+  toString() {}
 }
 
 const list = new linkedList();
-list.append("caine");
-list.append("pisica");
+list.append("dog");
+list.append("cat");
+list.append("kitty");
+
 console.log(list);
